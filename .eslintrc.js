4b343c4df6a96d2ce5719484f54ab247b684b990
Thibaud Desodt <tibo.desodt@gmail.com>
2020-12-03 21:41:46 +0100

    build(project): add linter

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:all",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+};
