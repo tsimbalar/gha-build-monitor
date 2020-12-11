e2db8bd9b243d2abfc95a3d4f86f60b3e0954ddf
Thibaud Desodt <tibo.desodt@gmail.com>
2020-12-11 23:46:54 +0100

    fix(request quotas): use throttling to avoid hitting github limitations

--- src/infra/github/OctokitFactory.ts
+++ src/infra/github/OctokitFactory.ts
@@ -1,5 +1,30 @@
 import { Octokit } from '@octokit/rest';
+import { throttling } from '@octokit/plugin-throttling';
+const MyOctokit = Octokit.plugin(throttling);
 
 export type OctokitFactory = (token: string) => Octokit;
 
-export const octokitFactory: OctokitFactory = (token) => new Octokit({ auth: token });
+export const octokitFactory: OctokitFactory = (token) => {
+  const octokit: Octokit = new MyOctokit({
+    auth: token,
+
+    throttle: {
+      onRateLimit: (retryAfter: any, options: any) => {
+        octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+
+        // Retry twice after hitting a rate limit error, then give up
+        if (options.request.retryCount <= 2) {
+          // eslint-disable-next-line no-console
+          console.log(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+        return undefined;
+      },
+      onAbuseLimit: (retryAfter: any, options: any) => {
+        // does not retry, only logs a warning
+        octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+      },
+    },
+  });
+  return octokit;
+};
