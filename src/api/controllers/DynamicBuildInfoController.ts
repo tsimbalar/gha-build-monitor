cab08bf544be078c87aaa70a2ec505f0625f6333
Thibaud Desodt <tibo.desodt@gmail.com>
2021-01-11 21:44:31 +0100

    feat(dynamic): deprecate /dynamic endpoint

--- src/api/controllers/DynamicBuildInfoController.ts
+++ src/api/controllers/DynamicBuildInfoController.ts
@@ -1,19 +1,24 @@
-import * as catlightCore from '../../catlight-protocol/shared';
 import * as catlightDynamic from '../../catlight-protocol/dynamic';
 import * as express from 'express';
-import { Body, Controller, Get, Post, Request, Response, Route, Security } from '@tsoa/runtime';
+import {
+  Body,
+  Controller,
+  Deprecated,
+  Get,
+  Post,
+  Request,
+  Response,
+  Route,
+  Security,
+} from '@tsoa/runtime';
 import {
   DynamicBuildInfoMetadataResponse,
   DynamicFilteredBuildInfoRequest,
   DynamicFilteredBuildInfoResponse,
 } from '../api-types';
-import { IRepoRepository, RepoName, Workflow } from '../../domain/IRepoRepository';
-import {
-  IWorkflowRunRepository,
-  WorkflowRun,
-  WorkflowRunsPerBranch,
-} from '../../domain/IWorkflowRunRepository';
-import { IAuthenticatedUser } from '../auth/IAuthentication';
+import { BuildInfoController } from './BuildInfoController';
+import { IRepoRepository } from '../../domain/IRepoRepository';
+import { IWorkflowRunRepository } from '../../domain/IWorkflowRunRepository';
 import { ValidationErrorJson } from '../middleware/schema-validation';
 
 interface ServerInfo {
@@ -22,50 +27,29 @@ interface ServerInfo {
   readonly version: string;
 }
 
-interface BuildDefinitionAndRuns {
-  readonly buildDefinition: catlightDynamic.BuildDefinitionStateRequest;
-  readonly runs: WorkflowRunsPerBranch;
-}
-
 @Route('dynamic')
 export class DynamicBuildInfoController extends Controller {
+  private readonly realController: BuildInfoController;
+
   public constructor(
     private readonly serverInfo: ServerInfo,
     private readonly repos: IRepoRepository,
     private readonly workflowRuns: IWorkflowRunRepository
   ) {
     super();
+    this.realController = new BuildInfoController(serverInfo, repos, workflowRuns);
   }
 
+  @Deprecated()
   @Get('')
   @Security('bearerAuth', ['repo'])
   public async getMetadata(
     @Request() request: express.Request
   ): Promise<DynamicBuildInfoMetadataResponse> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const currentUser = request.user!;
-    const userResponse = this.mapToUser(currentUser);
-
-    const repos = await this.repos.listForToken(currentUser.token);
-
-    return {
-      protocol: 'https://catlight.io/protocol/v1.0/dynamic',
-      id: this.serverInfo.id,
-      name: this.serverInfo.name,
-      serverVersion: this.serverInfo.version,
-      webUrl: 'http://myserver.example/dashboard',
-      currentUser: userResponse,
-      spaces: repos.map((repo) => ({
-        id: repo.name.fullName,
-        name: repo.name.fullName,
-        webUrl: repo.webUrl,
-        buildDefinitions: repo.workflows.map((wf) =>
-          this.mapToBuildDefinitionMetadata(repo.name, wf)
-        ),
-      })),
-    };
+    return this.realController.getMetadata(request);
   }
 
+  @Deprecated()
   @Post('')
   @Security('bearerAuth', ['repo'])
   @Response<ValidationErrorJson>(422, 'Validation error')
@@ -73,112 +57,6 @@ export class DynamicBuildInfoController extends Controller {
     @Request() request: express.Request,
     @Body() filters: DynamicFilteredBuildInfoRequest
   ): Promise<DynamicFilteredBuildInfoResponse> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const currentUser = request.user!;
-
-    const filteredBuilds = await this.getAllBuildsForFilter(currentUser.token, filters);
-
-    return {
-      protocol: 'https://catlight.io/protocol/v1.0/dynamic',
-      id: this.serverInfo.id,
-      spaces: filters.spaces.map((space) => ({
-        id: space.id,
-        name: space.id,
-        buildDefinitions: (filteredBuilds.get(space.id) || []).map((item) =>
-          this.mapToBuildDefinition(item)
-        ),
-      })),
-    };
-  }
-
-  private async getAllBuildsForFilter(
-    token: string,
-    filters: DynamicFilteredBuildInfoRequest
-  ): Promise<Map<string, ReadonlyArray<BuildDefinitionAndRuns>>> {
-    const getAllWorkflows = filters.spaces
-      .map((space) => {
-        const spaceId = space.id;
-        const repoName = RepoName.parse(spaceId);
-        const buildDefinitions = space.buildDefinitions;
-        return buildDefinitions.map(async (buildDef) => {
-          const runs = await this.workflowRuns.getLatestRunsForWorkflow(
-            token,
-            repoName,
-            buildDef.id,
-            {
-              maxAgeInDays: 3,
-              maxRunsPerBranch: 5,
-            }
-          );
-          return {
-            spaceId,
-            buildDefinition: buildDef,
-            runs,
-          };
-        });
-      })
-      .flatMap((x) => x);
-    const all = await Promise.all(getAllWorkflows);
-
-    const result = new Map<string, ReadonlyArray<BuildDefinitionAndRuns>>();
-    for (const item of all) {
-      const { spaceId, ...workflowAndRuns } = item;
-      const repoKey = spaceId;
-      const existing = result.get(repoKey) || [];
-
-      const updated = [...existing, workflowAndRuns];
-
-      result.set(repoKey, updated);
-    }
-    return result;
-  }
-
-  private mapToBuildDefinition(
-    buildDefinitionsAndRuns: BuildDefinitionAndRuns
-  ): catlightDynamic.BuildDefinitionStateResponse {
-    const buildDef = buildDefinitionsAndRuns.buildDefinition;
-    const runsPerBranch = buildDefinitionsAndRuns.runs;
-    return {
-      id: buildDef.id,
-      branches: this.mapToBuildBranches(runsPerBranch),
-    };
-  }
-
-  private mapToBuildBranches(runsPerBranch: WorkflowRunsPerBranch): catlightCore.BuildBranch[] {
-    return [...runsPerBranch.entries()].map<catlightCore.BuildBranch>(([branchName, runs]) => ({
-      id: branchName,
-      builds: runs.map((r) => this.mapToBuild(r)),
-    }));
-  }
-
-  private mapToBuild(run: WorkflowRun): catlightCore.Build {
-    return {
-      id: run.id,
-      startTime: run.startTime,
-      status: run.status,
-      finishTime: run.finishTime,
-      name: run.name,
-      webUrl: run.webUrl,
-      // TODO: contributors, triggeredByUser
-    };
-  }
-
-  private mapToBuildDefinitionMetadata(
-    repo: RepoName,
-    workflow: Workflow
-  ): catlightDynamic.BuildDefinitionMetadata {
-    return {
-      id: workflow.id,
-      name: `${repo.name} · ${workflow.name}`,
-      folder: repo.fullName,
-      webUrl: workflow.webUrl,
-    };
-  }
-
-  private mapToUser(authUser: IAuthenticatedUser): catlightCore.User {
-    return {
-      id: authUser.id,
-      name: authUser.name ?? authUser.login,
-    };
+    return this.realController.getServerState(request, filters);
   }
 }
